6cb44e7c2a79e1be9ec943aa2d1f9f56c71134db
unknown <lawzyaxy@gmail.com>
2025-03-24 18:11:26 +0000

    finished excercise 1

--- Program.cs
+++ Program.cs
@@ -3,15 +3,13 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {   
-        // user input for name request                     
+    {
         Console.Write("What is your first name? ");
         string first = Console.ReadLine();
 
         Console.Write("What is your last name? ");
         string last = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {last},{first} {last}.");
-        console.WriteLine("this c sharp assignment");   
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
-}   
+}
--- week02/Journal/week1.cs
+++ week02/Journal/week1.cs
@@ -4,14 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {   
-        // user input for name request                     
+        // user input request                     
         Console.Write("What is your first name? ");
         string first = Console.ReadLine();
 
         Console.Write("What is your last name? ");
         string last = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {last},{first} {last}.");
-        console.WriteLine("this c sharp assignment");   
+        Console.WriteLine($"Your name is {last}.");
     }
 }   
+
