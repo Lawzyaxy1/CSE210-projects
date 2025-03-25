4b995a9e3e3d55f18fdf02064e0820f6d3df88e4
unknown <lawzyaxy@gmail.com>
2025-03-25 19:07:37 +0000

    Finished Exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        // Welcome message
+        Console.WriteLine("Welcome to the program!");
+        
+        // Ask for the user's name
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        
+        // Ask for the user's favorite number
+        Console.Write("Please enter your favorite number: ");
+        int favoriteNumber = int.Parse(Console.ReadLine());
+        
+        // Calculate the square of the number
+        int square = favoriteNumber * favoriteNumber;
+        
+        // Display the result with the user's name
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
